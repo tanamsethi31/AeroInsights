d2963ad403b3cdfc27cd66f7d462fb3a730ab191
Tanam Sethi <tanamsethi@Tanams-MacBook-Air.local>
2026-05-07 18:50:18 +0100

    feat: install bootstrap-icons and import CSS globally

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./app/App.tsx";
 import "./styles/index.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const domain   = import.meta.env.VITE_AUTH0_DOMAIN as string;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID as string;
